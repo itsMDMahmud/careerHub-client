37ba7ec271dd277d4c434aad2b1b22c6f2efb544
itsMDMahmud <itsmdmahmud@gmail.com>
2023-04-09 21:31:46 +0600

    first commit

---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <H1>this is home</H1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/component/Main/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Main.css'
+import Nav from '../Nav/Nav';
+import Footer from '../Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            <h2>this is main</h2>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
